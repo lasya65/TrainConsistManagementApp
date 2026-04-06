676ede8701b5a58641b7e1956adcf19d8a9616e2
lasya65 <sp9174@srmist.edu.in>
2026-04-06 16:25:45 +0530

    UC1

---
+++ App/src/UC1.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class UC1 {
+        public static void main(String[] args){
+
+            System.out.println("=========================================================");
+            System.out.println("==============Train-Consist-Management-App===============");
+            System.out.println("=========================================================");
+
+            List<String> trainConsist = new ArrayList<>();
+
+            System.out.println("Initial Bogie Count : "+trainConsist.size());
+            System.out.println("Current Train Consist : "+trainConsist);
+
+            System.out.println("Train initialized successfully...");
+        }
+    }
+
+
+}
